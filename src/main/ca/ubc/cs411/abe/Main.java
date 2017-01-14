f8fcdead4286617e84d4d5e112ce5987ab581899
Mark Sayson <masayson@gmail.com>
2017-01-13 18:54:39 -0800

    Added example expressions with interp() and typeOf() results to Main printout

--- src/main/ca/ubc/cs411/abe/Main.java
+++ src/main/ca/ubc/cs411/abe/Main.java
@@ -5,12 +5,17 @@ import java.util.Arrays;
 public class Main {
 
     private static void printArithmeticExpressionsRawAndInterpreted() {
-        ABE e1 = new Num(7);
-        ABE e2 = new Add(new Num(7), new Num(4));
-        ABE e3 = new Sub(new Add(new Num(7), new Num(4)), new Num(3));
-        for (ABE e : Arrays.asList(e1, e2, e3)) {
+        ABE num = new Num(7);
+        ABE add = new Add(new Num(7), new Num(4));
+        ABE sub = new Sub(new Add(new Num(7), new Num(4)), new Num(3));
+        ABE tru = new True();
+        ABE fls = new False();
+        ABE if1 = new If(tru, add, sub);
+        ABE if2 = new If(fls, add, sub);
+        ABE if3 = new If(tru, fls, tru);
+        for (ABE e : Arrays.asList(num, add, sub, tru, fls, if1, if2, if3)) {
             System.out.println(e);
-            System.out.println(e.interp());
+            System.out.println("  interprets to: " + e.interp() + ", has type: " + e.typeOf());
         }
     }
 
