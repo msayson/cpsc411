5ef1ee7fb35464ea97862e8bb5818a4a5d097623
Mark Sayson <masayson@gmail.com>
2017-01-19 16:39:05 -0800

    Updated generic symbol for consistency across the project

--- src/main/ca/ubc/cs411/abevisitor/visitor/Visitor.java
+++ src/main/ca/ubc/cs411/abevisitor/visitor/Visitor.java
@@ -2,11 +2,11 @@ package ca.ubc.cs411.abevisitor.visitor;
 
 import ca.ubc.cs411.abevisitor.expression.*;
 
-public interface Visitor<X> {
-    X visit(Num n);
-    X visit(Add a);
-    X visit(Sub s);
-    X visit(True t);
-    X visit(False f);
-    X visit(If i);
+public interface Visitor<T> {
+    T visit(Num n);
+    T visit(Add a);
+    T visit(Sub s);
+    T visit(True t);
+    T visit(False f);
+    T visit(If i);
 }
